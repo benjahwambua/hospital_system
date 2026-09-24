20c073b501cb24c4404c32ea33691d50babf83fa
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-24 23:00:53 +0300

    Add cashier payment history to finance menu

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -352,6 +352,9 @@ function isParentActive($paths) {
         <a href="/hospital_system/cashier/shifts.php" class="<?= isActive('cashier/shifts.php') ?>">
             <i class="fas fa-clock icon-main"></i> Cashier Shift
         </a>
+        <a href="/hospital_system/cashier/payment_history.php" class="<?= isActive('cashier/payment_history.php') ?>">
+            <i class="fas fa-receipt icon-main"></i> Payment History
+        </a>
         <?php endif; ?>
 
         <div class="menu-title">Exit</div>
