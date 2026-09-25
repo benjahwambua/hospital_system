e37fa76eea8470eb18600026d0fa75d44edfb874
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:39 +0300

    security: enforce role authorization on protected writes

--- vitals/vitals_add.php
+++ vitals/vitals_add.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_once __DIR__ . '/../includes/auth.php';
+require_role(['admin','doctor','nurse']);
 
 $patient_id = intval($_GET['patient_id'] ?? 0);
 $msg = "";
