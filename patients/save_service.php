a98c90c332946a5faef43eeb58ff96c9af2f8864
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:33 +0300

    security: enforce role authorization on protected writes

--- patients/save_service.php
+++ patients/save_service.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_role(['admin','doctor','nurse']);
 
 $patient_id = intval($_POST['patient_id'] ?? 0);
 $service_id = intval($_POST['service_id'] ?? 0);
