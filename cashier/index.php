7a83d2518fd35090edf533e828ee33d275be6dc7
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 14:33:01 +0300

    Load cashier shift helper in Central Cashier

--- cashier/index.php
+++ cashier/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../helpers/billing.php';
+require_once __DIR__ . '/../helpers/cashier.php';
 require_login();
 
 $role = strtolower(trim((string)($_SESSION['role'] ?? '')));
