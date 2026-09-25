b63df806be4feacbb4f7fc75453173987f69d02d
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 10:42:33 +0300

    Restore pharmacy Sell Medicine module

--- pharmacy/sell_medicine.php
+++ pharmacy/sell_medicine.php
@@ -3,6 +3,6 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_login();
-require_role(['pharmacist','admin']);
-header('Location: /hospital_system/pharmacy/dispensing_queue.php?notice=central_cashier');
+require_role(['pharmacist','admin','cashier']);
+header('Location: /hospital_system/pharmacy/walkin_sale.php');
 exit;
