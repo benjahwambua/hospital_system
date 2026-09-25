f5fc5b4ed56ba4b028ae5ddd30dcd1935f89fc3b
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 11:53:08 +0300

    Use centralized session bootstrap in header

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
 // includes/header.php
-if (session_status() === PHP_SESSION_NONE) session_start();
+require_once __DIR__ . '/session.php';
 require_once __DIR__ . '/../config/config.php';
 ?>
 <!doctype html>
