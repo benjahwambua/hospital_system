12ccb0d6a73cbdfc98b8357e80a1328341059f93
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:36 +0300

    security: enforce role authorization on protected writes

--- patients/remove_item.php
+++ patients/remove_item.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_role(['admin']);
 
 $id = intval($_GET['id']);
 $type = $_GET['type']; // 'service' or 'prescription'
