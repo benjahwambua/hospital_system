6678b153ff5c606f06cedfa7f39d9a8ee4f22078
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:12 +0300

    security: enforce role authorization on clinical write endpoints

--- doctors/add_order.php
+++ doctors/add_order.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/includes/session.php';
+require_once __DIR__ . '/includes/auth.php';
 require_login();
+require_role(['admin','doctor']);
 
 include __DIR__ . '/includes/header.php';
 include __DIR__ . '/includes/sidebar.php';
