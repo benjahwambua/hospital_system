ece97870260e6d9ae5892f2088ecc3ee03a43082
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:09 +0300

    security: enforce role authorization on clinical write endpoints

--- doctors/prescribe.php
+++ doctors/prescribe.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../includes/auth.php';
 require_login();
+require_role(['admin','doctor']);
 
 // Get patient
 $patient_id = intval($_GET['id'] ?? 0);
