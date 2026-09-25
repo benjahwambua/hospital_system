2fa2c1b2f599855d6ce89e4e1e5c88850462463f
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:01:47 +0300

    security: enforce role authorization on protected writes

--- maternity/add_bill.php
+++ maternity/add_bill.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_once __DIR__ . '/../includes/auth.php';
+require_role(['admin','cashier','accountant']);
 
 $maternity_id = intval($_GET['id'] ?? 0);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
