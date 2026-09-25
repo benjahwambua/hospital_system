d146ea483e5ac1d68b44585f6968d7f3d70755a3
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:00:51 +0300

    security: enforce role authorization on clinical write endpoints

--- ajax/save_billing.php
+++ ajax/save_billing.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../includes/auth.php';
 require_login();
+require_role(['admin','cashier','accountant']);
 
 $patient_id = intval($_POST['patient_id'] ?? 0);
 $item = trim($_POST['item'] ?? '');
