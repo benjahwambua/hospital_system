d3450b953516dc689601728ada42a1fc5ca37f5a
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:12:48 +0300

    CSRF hardening: protect pharmacy supplier creation

--- pharmacy/vendors.php
+++ pharmacy/vendors.php
@@ -6,6 +6,7 @@ require_login();
 require_role(['admin']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!verify_csrf_token($_POST['csrf_token'] ?? null)) { http_response_code(419); exit('Invalid security token.'); }
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $category = $_POST['category'];
@@ -21,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="card-header bg-primary text-white"><h5>Register New Supplier</h5></div>
     <div class="card-body">
         <form method="POST">
+            <input type="hidden" name="csrf_token" value="<?= htmlspecialchars(csrf_token(), ENT_QUOTES, "UTF-8") ?>">
             <div class="row">
                 <div class="col-md-4"><label>Supplier Name</label><input type="text" name="name" class="form-control" required></div>
                 <div class="col-md-4"><label>Phone Number</label><input type="text" name="phone" class="form-control"></div>
