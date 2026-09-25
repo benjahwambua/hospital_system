22a95592254ed5ca76025479a490298dd3a7a2cb
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:08:05 +0300

    security: add CSRF tokens to forms

--- pharmacy/add_medication.php
+++ pharmacy/add_medication.php
@@ -16,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
 <div class="card"><h3>Add Medication</h3>
 <?php if($msg) echo "<div class='alert'>$msg</div>"; ?>
 <form method="post">
+<input type="hidden" name="csrf_token" value="<?= htmlspecialchars(csrf_token(), ENT_QUOTES, 'UTF-8') ?>">
 <label>Name</label><input name="name" class="form-control" required>
 <label>Description</label><textarea name="description" class="form-control"></textarea>
 <label>Buy Price</label><input name="buy_price" class="form-control" type="number" step="0.01">
