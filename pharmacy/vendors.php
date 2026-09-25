8749435cb4de0410c5e7574d1e4b1e25991a05a9
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-25 12:02:31 +0300

    security: enforce role authorization

--- pharmacy/vendors.php
+++ pharmacy/vendors.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__.'/../config/config.php';
-// ... standard header/sidebar includes ...
+require_once __DIR__.'/../includes/session.php';
+require_once __DIR__.'/../includes/auth.php';
+require_login();
+require_role(['admin']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
