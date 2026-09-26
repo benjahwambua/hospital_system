078fa2effa43d574450e555791e8c7e21e721b44
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:48:19 +0300

    Treat radiology result save as edit permission

--- ajax/save_radiology.php
+++ ajax/save_radiology.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_login();
-require_module_access($conn, 'radiology', 'create');
+require_module_access($conn, 'radiology', 'edit');
 require_role(['admin','doctor','nurse']);
 
 $response = ['success'=>false,'message'=>'Invalid request'];
