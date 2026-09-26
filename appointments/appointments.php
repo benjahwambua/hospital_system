380d3f716a219c5a3656e14b0e39238ac4d2967e
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:13:49 +0300

    Enforce action-level module permissions on transactions

--- appointments/appointments.php
+++ appointments/appointments.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_module_access($conn, 'clinical', 'create');
 
 $error = '';
 $success = false;
