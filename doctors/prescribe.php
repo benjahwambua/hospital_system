4a3b424d5fd7b2cc06b9431bc4094eab59009d5e
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 22:29:14 +0300

    Tighten clinical action permissions

--- doctors/prescribe.php
+++ doctors/prescribe.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_login();
+require_module_access($conn, 'clinical', 'create');
 require_role(['admin','doctor']);
 
 // Get patient
