17b9d70027f5661cd5346163e52cbb242c168932
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:20:22 +0300

    Enforce pharmacy action-level permissions

--- pharmacy/walkin_sale.php
+++ pharmacy/walkin_sale.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../helpers/billing.php';
 require_login();
+require_module_access($conn, 'pharmacy', 'create');
 require_role(['admin','pharmacist','cashier']);
 
 if (empty($_SESSION['csrf_token'])) $_SESSION['csrf_token']=bin2hex(random_bytes(32));
