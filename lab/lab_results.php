35ebeeb9e5ffa1ac16f35e9eb0ffb039c8314cca
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:20:43 +0300

    Enforce laboratory action permissions

--- lab/lab_results.php
+++ lab/lab_results.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_module_access($conn, 'laboratory', 'approve');
 
 if (empty($_SESSION['csrf_token'])) $_SESSION['csrf_token']=bin2hex(random_bytes(32));
 $csrfToken=$_SESSION['csrf_token'];
