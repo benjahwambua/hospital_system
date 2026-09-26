b0f1e22c8a455f239c9e006de86b71f9eacce1d0
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:34:57 +0300

    Harden finance and supplier permission enforcement

--- procurement/add_vendor.php
+++ procurement/add_vendor.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../config/config.php';
 require_once __DIR__.'/../includes/session.php';
 require_once __DIR__.'/../includes/auth.php';
 require_login();
+require_module_access($conn, 'procurement', 'create');
 require_role(['admin']);
 
 $message = '';
