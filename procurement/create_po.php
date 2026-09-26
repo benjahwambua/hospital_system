cac11860773c721f677ae820cc67366c09368917
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:13:33 +0300

    Enforce action-level module permissions on transactions

--- procurement/create_po.php
+++ procurement/create_po.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_module_access($conn, 'procurement', 'create');
 require_role(['admin']);
 
 if (empty($_SESSION['csrf_token'])) {
