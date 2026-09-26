cc2850e69600e8f9f8a14d4df08e86e1eb36a12a
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:45:37 +0300

    Harden administration permissions and settings actions

--- settings/system_settings.php
+++ settings/system_settings.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../includes/session.php';
-require_login();
-require_once __DIR__ . '/../includes/auth.php';
-require_role(['admin','accountant']);
+undefined
 
 $message = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
