06e9ba5fb249823dbed53f3c671ecfb97476cedd
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:20:15 +0300

    Enforce pharmacy action-level permissions

--- pharmacy/adjust_price.php
+++ pharmacy/adjust_price.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_module_access($conn, 'pharmacy', 'edit');
 
 /* Get medicine ID */
 $id = intval($_GET['id'] ?? 0);
