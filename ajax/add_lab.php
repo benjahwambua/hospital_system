3676d142a6f3b103438548ed968e182eb7adc0b3
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:34:16 +0300

    Harden module action permissions on clinical, lab, radiology and pharmacy endpoints

--- ajax/add_lab.php
+++ ajax/add_lab.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../helpers/billing.php';
 require_login();
+require_module_access($conn, 'laboratory', 'create');
 require_role(['admin','doctor','nurse']);
 
 header('Content-Type: application/json');
