d1e453b0bc870fac997c73a188330728a7b8b758
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:35:00 +0300

    Harden finance and supplier permission enforcement

--- procurement/manage_suppliers.php
+++ procurement/manage_suppliers.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_login();
+require_module_access($conn, 'procurement', 'view');
 require_role(['admin']);
 
 if (empty($_SESSION['csrf_token'])) {
