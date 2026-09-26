95e37e0283bd5cbc11703f6e388256d01e630fc8
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:20:18 +0300

    Enforce pharmacy action-level permissions

--- pharmacy/stock_take.php
+++ pharmacy/stock_take.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_login();
+require_module_access($conn, 'pharmacy', 'edit');
 require_role(['admin','pharmacist']);
 if (empty($_SESSION['csrf_token'])) $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 
