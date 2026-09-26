1370309e10252a1236c751ecbaa003e348d58818
Benjamin Wambua <wambuabenjah@gmail.com>
2026-09-26 21:13:39 +0300

    Enforce action-level module permissions on transactions

--- procurement/receive_inventory.php
+++ procurement/receive_inventory.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_login();
+require_module_access($conn, 'procurement', 'create');
 require_role(['admin']);
 
 if (empty($_SESSION['csrf_token'])) $_SESSION['csrf_token']=bin2hex(random_bytes(32));
